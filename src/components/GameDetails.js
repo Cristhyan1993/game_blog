138395abca703abe8779f9cde0cd40a12433fb1a
Cristhyan <cristhyancalderon@hotmail.co.uk>
2024-09-12 14:06:27 +0100

    game-details styling changed

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -9,9 +9,31 @@ import './GameDetails.css'
 function GameDetails(props) {
   return (
     <div className='game-details'>
-        <h1>{props.gameDetails.title}</h1>
-        <h3>Platform: <FontAwesomeIcon icon={faPlaystation}/></h3>
-        <p>{props.gameDetails.overview}</p>
+      <h1>{props.gameDetails.title}</h1>
+      <h3>Platform: <FontAwesomeIcon icon={faPlaystation} /></h3>
+      <h2>Game Overview:</h2>
+      <p>{props.gameDetails.gameReview.gameOverview}</p>
+      <h2>Gameplay:</h2>
+      <p>{props.gameDetails.gameReview.gameplay}</p>
+      <h2>Controls:</h2>
+      <p>{props.gameDetails.gameReview.controls}</p>
+      <h2>Difficulty:</h2>
+      <p>{props.gameDetails.gameReview.difficulty.description}</p>
+      <h2>Multiplayer:</h2>
+      <p>{props.gameDetails.gameReview.multiplayer}</p>
+      <h2>Conclusion:</h2>
+      <p>{props.gameDetails.gameReview.conclusion.description}</p>
+
+      <div className='ratings-container'>
+        <div className='ratings-item'>
+          <h2>My Score:</h2>
+          <p>{props.gameDetails.gameReview.difficulty.rating}/10 <i className="fa-solid fa-star star"/></p>
+        </div>
+        <div className='ratings-item'>
+          <h2>Difficulty rating:</h2>
+          <p>{props.gameDetails.gameReview.conclusion.score}/10 <i className="fa-solid fa-fire fire"/></p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Ctr.js
+++ src/pages/Ctr.js
@@ -9,7 +9,20 @@ function Ctr() {
     title: "Crash Team Racing Nitro fueled",
     imgCover: "images/Crash-Team-Racing-Nitro-Fueled.jpg",
     heroText: "Developer time trials and more...",
-    overview:"Having completed the main story including the platimun relics, CTR challanges and time trials. i tackled all the developer time trials in order to obtain the platinum playstation trophy. You can view my developer time trials in the videos below."
+    gameReview: {
+      gameOverview:'Rev up your engines, "Crash Team Racing Nitro-Fueled" brings a polished homage to the beloved 1999 classic. This remake not only revitalizes the iconic kart racer but adds enough modern tweaks to make it a must-play for both long-time fans and newcomers alike.',
+      gameplay:"The game modes offer a satisfying blend of nostalgia and innovation, with Story Mode and Time Trials feeling familiar yet fresh. Graphically, the game is a visual treat. The updated textures and vibrant colours breathe new life into the tracks and characters. Each course is meticulously designed, and the character models are both charming and detailed. The soundtrack, a mix of retro and new tunes, complements the action perfectly.",
+      controls: "From the moment you start racing, it's clear that the controls are as tight and responsive as ever. The PS4 controller feels perfectly suited for the high-speed thrills of Crash Team Racing.",
+      difficulty: {
+        description:"The difficulty scales depending on the game mode. Story Mode being beginner friendly, the CTR and Time Relics present a challenge however the completing the time trials are definitely the biggest challenges.",
+        rating: 8
+      },
+      multiplayer:"The local multiplayer definitely shone brighter in comparison to online. You can quickly jump into any race of choice and the gameplay is much smoother. I found the online mode a bit disappointing, mainly due to the lobby waiting time, the number of players disconnecting mid race and a few bugs.",
+      conclusion:{
+        description:"Overall, “Crash Team Racing Nitro-Fueled” is a thrilling and well-executed remake that pays homage to its roots while offering something new. Whether you're a fan of the original or just looking for a fun kart racer, this game is definitely worth the ride. Having completed the main story including the platinum relics, CTR challenges and time trials. I tackled all the developer time trials in order to obtain the platinum PlayStation trophy. You can view my developer time trials in the videos below.'",
+        score: 8
+      }
+    },
   }
 
   const ctrCards = [
