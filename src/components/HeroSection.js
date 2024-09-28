3cf2abdfb8a005ca1715f5200e813c8b888910ae
Cristhyan <cristhyancalderon@hotmail.co.uk>
2024-09-28 12:17:56 +0100

    redone hero Section again

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,17 +1,41 @@
 import React from "react";
 import './HeroSection.css'
+import { HashLink } from "react-router-hash-link";
 
 function HeroSection(props) {
+  const scrollWithOffset = (el, offset) => {
+    const elementPosition = el.offsetTop - offset;
+    window.scroll({
+      top: elementPosition,
+      left: 0,
+      behavior: "smooth"
+    });
+  }
   return (
     <div className="hero-container">
-        <img src={props.gameDetails.images.cover} alt="game wallpaper" className="hero-image"/>
-      <div className="hero-title">
-      {props.gameDetails.logo ?
-        <img src={props.gameDetails.logo} alt="website logo" id="image-logo"/>
-        :
-        <h1>{props.gameDetails.title}</h1>
+      <div className="hero-content">
+        <img src={props.gameDetails.images.cover} alt="game wallpaper" className="hero-image" />
+        {props.gameDetails.logo ?
+          <div className="hero-context">
+            <h1>Gameplay & Honest Reviews</h1>
+            <h2>{props.gameDetails.heroText}</h2>
+            <HashLink
+              to="/#cards"
+              scroll={el => scrollWithOffset(el, 80)}
+            >
+              <button class="pushable">
+                <span class="front">
+                  Favourite Games
+                </span>
+              </button>
+            </HashLink>
+          </div>
+          :
+          <div className="hero-context">
+            <h1>{props.gameDetails.title}</h1>
+            <h2>{props.gameDetails.heroText}</h2>
+          </div>
         }
-        <h2 >{props.gameDetails.heroText}</h2>
       </div>
     </div>
   );
